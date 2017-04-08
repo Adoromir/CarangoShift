718578a80da0fce0d4c9b5507b51818432d99c55
logonrm <logonrm@fiap.com.br>
2017-04-08 17:48:36 -0300

    Criacao do projeto de carros

---
+++ app/src/main/java/br/com/edimar/carango/api/CarroAPI.java
@@ -0,0 +1,20 @@
+package br.com.edimar.carango.api;
+
+import java.util.List;
+
+import br.com.edimar.carango.model.Carro;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * Created by logonrm on 08/04/2017.
+ */
+
+public interface CarroAPI {
+    //http://heiderlopes.com.br/carros/tipo/classicos.json
+
+    @GET("/carros/tipo/{tipo}.json")
+    Call<List<Carro>> buscarCarros(
+            @Path("tipo") String tipo);
+}
---
+++ app/src/main/java/br/com/edimar/carango/listener/OnClickListener.java
@@ -0,0 +1,13 @@
+package br.com.edimar.carango.listener;
+
+import android.view.View;
+
+/**
+ * Created by logonrm on 08/04/2017.
+ */
+
+public interface OnClickListener {
+
+    public void onClick(View v, int position);
+
+}
